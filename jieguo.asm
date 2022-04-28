acb7d19420333dd9e5fd507201a601a312967738
gaozhenyue <gzynanyou@Outlook.com>
2022-04-28 19:15:21 +0800

    完美成功，大功告成。

--- jieguo.asm
+++ jieguo.asm
@@ -4,7 +4,20 @@ data segment
 dw 0,0
 dw 200,200
 db 0001b
+        mus_freg  dw 330,294,262,294,3 dup (330)     ;频率表
+                dw 3 dup (294),330,392,392
+                dw 330,294,262,294,4 dup (330)
+                dw 294,294,330,294,262,-1
+        mus_time  dw 6 dup (25),50                   ;节拍表
+                dw 2 dup (25,25,50)
+                dw 12 dup (25),100
 data ends
+
+;----------音乐地址宏-----------
+ADDRESS MACRO A,B
+     LEA SI,A
+     LEA BP,DS:B
+ENDM
 
 stack segment
 db 128 dup (0)
@@ -23,7 +36,11 @@ start:
         mov ax,0
         mov es,ax
 
-try:    push es:[4*9]
+        
+
+try:    
+        address mus_freg, mus_time
+        push es:[4*9]
         pop ds:[0]
         push es:[4*9+2]
         pop ds:[2]      ;备份原来int 9,好像用不到，不改了
@@ -32,18 +49,29 @@ try:    push es:[4*9]
         mov es:[4*9+2],cs
 
         call wujiaoxin
-        ;call zhongduan
+        
+        call music
+
         push ds:[0]
         pop es:[9*4]
         push ds:[2]
-        pop es:[9*4+2]
+        pop es:[9*4+2]  ;回复原本的int 9中断，否则会无法调用第二，总之会发生莫名奇妙的错误
+
+
         jmp try
 
-; int9:
-;         call zhongduan
-;         jmp try
+
 
 wujiaoxin proc
+
+        push si
+        push di
+        push cx
+        push dx
+        push ax
+        push bx
+        push es
+
 work:   mov al,12h      ;640*480 256的图形模式:  
         mov ah,0        ;是用来设定显示模式的服务程序
         mov cx,ds:[4]
@@ -115,9 +143,23 @@ huitu5:
         dec bx
         cmp bx,0
         jne huitu5
+
+        pop es
+        pop bx
+        pop ax
+        pop dx
+        pop cx
+        pop di
+        pop si
+
+
         ret
 wujiaoxin endp
 zhongduan: 
+        push si
+        push di
+        push cx
+        push dx
         push ax
         push bx
         push es
@@ -151,6 +193,10 @@ jp5:
         pop es
         pop bx
         pop ax
+        pop dx
+        pop cx
+        pop di
+        pop si
         call wujiaoxin
         iret
         
@@ -190,6 +236,77 @@ space:
         inc bx
         mov ds:[8],bx
         jmp jp5
+
+;------------发声-------------
+gensound proc near
+     push ax
+     push bx
+     push cx
+     push dx
+     push di
+
+     mov al, 0b6H
+     out 43h, al    ;初始化8253，用于产生频率所需方波
+
+     mov dx, 12h
+     mov ax, 348ch
+     div di         ;计算要产生的声音频率
+
+     out 42h, al    
+     mov al, ah
+     out 42h, al    ;将频率传到42端口，以便计算出相应的值
+
+     in al, 61h
+     mov ah, al
+     or al, 3       ;将PB0,PB1两位置1，发声音
+     out 61h, al
+wait1:
+     mov cx, 3314
+     call waitf
+delay1:
+     dec bx
+     jnz wait1
+
+     mov al, ah
+     out 61h, al
+
+     pop di
+     pop dx
+     pop cx
+     pop bx
+     pop ax
+     ret 
+gensound endp
+
+;--------------------------
+waitf proc near
+      push ax
+waitf1:
+      in al,61h
+      and al,10h
+      cmp al,ah
+      je waitf1
+      mov ah,al
+      loop waitf1
+      pop ax
+      ret
+waitf endp
+;--------------发声调用函数----------------
+music proc near
+      xor ax, ax
+freg:
+      mov di, [si]
+      cmp di, 0FFFFH
+      je end_mus
+      mov bx, ds:[bp]
+      
+      call gensound
+      add si, 2
+      add bp, 2
+      jmp freg
+end_mus:
+      ret
+music endp
 
 code ends
 end start
